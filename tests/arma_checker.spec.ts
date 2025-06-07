a6f4b55decb05ad3a092d1e22cb3948fe3352583
Kevin <kevin.ortiz.software@gmail.com>
2025-06-07 19:55:53 -0400

    Testing

--- tests/arma_checker.spec.ts
+++ tests/arma_checker.spec.ts
@@ -181,7 +181,7 @@ for (const { id, name } of workshopMods) {
     const diffMs = now.getTime() - lastUpdated.getTime();
     let diffHours = diffMs / (1000 * 60 * 60);
     const isRecent = diffHours < Hours_ThresHold;
-    diffHours -= 3;
+    diffHours;
     const ageHours = diffHours.toFixed(1);
 
     if (isRecent) {
