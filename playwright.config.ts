cb8a80f00ac7032e85b62fe2e6e11de7d5938046
Kevin <kevin.ortiz.software@gmail.com>
2026-06-09 17:00:16 -0400

    updating the path of the arma_checker.spec.ts in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./playwright/tests",
+  testDir: "./tests",
   testMatch: "**/*.spec.ts",
   timeout: 60_000,
-  reporter: [["list"], ["html", { outputFolder: "playwright/playwright-report" }]],
+  reporter: [["list"], ["html", { outputFolder: "playwright-report" }]],
   use: {},
   projects: [
     {
